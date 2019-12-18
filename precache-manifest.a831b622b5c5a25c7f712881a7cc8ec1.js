88c937bad2cd557125e42170757eed170102981d
acarlie <ameliacarlie@gmail.com>
2019-12-18 12:55:26 -0500

    Updates

---
+++ precache-manifest.a831b622b5c5a25c7f712881a7cc8ec1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ba1fc0b9420c5173d72c2e1888d8f9",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "6fa34358600d0e5c9eeb",
+    "url": "/react_portfolio/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "6a3fa2ab923adfdc24ce",
+    "url": "/react_portfolio/static/css/main.7e9467d5.chunk.css"
+  },
+  {
+    "revision": "6fa34358600d0e5c9eeb",
+    "url": "/react_portfolio/static/js/2.57160ef1.chunk.js"
+  },
+  {
+    "revision": "c65614ba1b7c3b5134a1ed89f56b30da",
+    "url": "/react_portfolio/static/js/2.57160ef1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a3fa2ab923adfdc24ce",
+    "url": "/react_portfolio/static/js/main.1ac848c1.chunk.js"
+  },
+  {
+    "revision": "2666c572e91cae50d314",
+    "url": "/react_portfolio/static/js/runtime-main.07f2c53f.js"
+  },
+  {
+    "revision": "d5a59ed9304e8d327bba63f78c78c348",
+    "url": "/react_portfolio/static/media/profile_square.d5a59ed9.png"
+  }
+]);
